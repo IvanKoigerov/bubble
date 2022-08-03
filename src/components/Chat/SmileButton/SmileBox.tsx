c9f533d1b1c7cabe83d6ecea1af9c70fe3991c42
Ivan Koigerov <heartsf0xs@gmail.com>
2022-08-03 18:31:33 +0300

    feat(Smile): add smile

--- src/components/Chat/SmileButton/SmileBox.tsx
+++ src/components/Chat/SmileButton/SmileBox.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SmileProps } from './SmileButton';
 
-interface SmileProps {
-  addSmile: (smile?: string) => void;
+interface SmileProp extends SmileProps {
+  isOpen: boolean;
 }
 
 const smileArr = ['😊', '😂', '😘', '😎', '😱', '😡', '😢', '😐', '👋', '👍', '👎', '❤'];
 
-const SmileBox = (props: SmileProps) => {
+const SmileBox = (props: SmileProp) => {
   return (
-    <SmileWrapper>
+    <SmileWrapper isOpen={props.isOpen}>
       {smileArr.map((smile, key) => (
         <SmileButton onClick={() => props.addSmile(smile)} key={key}>
           {smile}
@@ -19,20 +20,23 @@ const SmileBox = (props: SmileProps) => {
   );
 };
 
-const SmileWrapper = styled.div`
+const SmileWrapper = styled.div<{ isOpen?: boolean }>`
   position: absolute;
-  top: -160px;
+  top: -150px;
   width: 200px;
-  height: 150px;
   border: 1px solid ${(props) => props.theme.input};
   background: ${(props) => props.theme.chatColor};
   box-shadow: ${(props) => props.theme.bubbleShadow};
   border-radius: 2px;
-  padding: 10px;
   font-size: 22px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  padding: 10px;
   user-select: none;
+  transition: 0.3s ease;
+  opacity: ${(props) => (props.isOpen ? '1' : '0')};
+  visibility: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
+  transform: ${(props) => (props.isOpen ? 'translateY(0)' : 'translateY(-30px)')};
 `;
 
 const SmileButton = styled.span`
@@ -40,6 +44,7 @@ const SmileButton = styled.span`
   border-radius: 5px;
   cursor: pointer;
   text-align: center;
+  line-height: 1.3;
 
   &:hover {
     background: ${(props) => props.theme.input};
